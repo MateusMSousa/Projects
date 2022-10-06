27707c24e929025e1477648b40caeeee5ca0f94c
MateusMSousa <57472311+MateusMSousa@users.noreply.github.com>
2022-10-06 14:57:08 -0300

    Perfeito
    Programa que recebe um numero e diz se é perfeito.

--- NumPerfeito.cpp
+++ NumPerfeito.cpp
@@ -11,25 +11,25 @@ int main()
     scanf("%d", &num);
     //Confere se num = 0, se sim imprime erro e tenta novamente.
     while(num == 0){
-        printf("Erro! Valor n„o pode ser 0\n");
+        printf("Erro! Valor n√£o pode ser 0\n");
         printf("Digite um numero:");
         scanf("%d", &num);
     }
-    //Inicia a int total, que È o total da soma dos divisores.
+    //Inicia a int total, que √© o total da soma dos divisores.
     int total = 0;
     //Faz o loop para todos os numeros anteriores a num.
     for(int i = 1; i < num; i++){
-        //Se o resto de num dividido por i for 0, ent„o i È um divisor de num.
+        //Se o resto de num dividido por i for 0, ent√£o i √© um divisor de num.
         if(num % i == 0){
             total = total + i;
         }
         }
-        //Se total = num, ent„o num È perfeito
+        //Se total = num, ent√£o num √© perfeito
         if(total == num){
             printf("%i e perfeito\n", num);
             return true;
         }
-        //Se total != num, ent„o num nao e perfeito
+        //Se total != num, ent√£o num nao e perfeito
         else{
             printf("%i nao e perfeito\n", num);
             return false;
